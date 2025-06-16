cae7bcbe59f72bbeb662ed555b8d3baad1305b70
Alan Koncar <koncar.alan@gmail.com>
2025-06-15 22:39:50 -0400

    static code analysis improvements

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,36 +1,44 @@
-import requests
+"""Function definitions to run the emotion detection API and formatting helpers
+"""
 import json
+import requests
 
 def emotion_detector(text_to_analyse):
+    """Emotion detection API call including error handling for empty input
+    """
     url = 'https://sn-watson-emotion.labs.skills.network/' + \
           'v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyse } }
-    response = requests.post(url, json=input_json, headers=headers)
+    response = requests.post(url, json=input_json, headers=headers, timeout=100)
     if response.status_code == 400:
-        no_text = {"anger": None, 
-                   "disgust": None, 
-                   "fear": None, 
-                   "joy": None, 
-                   "sadness": None, 
+        no_text = {"anger": None,
+                   "disgust": None,
+                   "fear": None,
+                   "joy": None,
+                   "sadness": None,
                    "dominant_emotion": None}
         return no_text
     return format_response(response.text)
 
 def format_response(response_to_format):
+    """formatting response of the API call to return JSON 
+    and add "dominant_emotion" key
+    """
     dictionary = json.loads(response_to_format)
     formated_response = dictionary["emotionPredictions"][0]["emotion"]
     formated_response['dominant_emotion'] = max(formated_response, key=formated_response.get)
     return formated_response
 
 def format_final_statement(response):
+    """Formats response for the final display on index.html
+    """
     dominant_emotion = response["dominant_emotion"]
-    
     emotions = []
     for item in response:
         if item == "dominant_emotion":
             continue
-        emotions.append(f"'{item}': {response[item]}")    
+        emotions.append(f"'{item}': {response[item]}")
     result_statement = ", ".join(emotions)
 
     last_comma_index = result_statement.rfind(',')
@@ -39,6 +47,4 @@ def format_final_statement(response):
 
     final_statement = f"<p>For the given statement, the system response is {result_statement}. " \
                       + f"The dominant emotion is <b>{dominant_emotion}</b>.</p>"
-    
     return final_statement
-
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""This module deploys the funcionality of the emotion detection 
+to a local host and handles errors for empty input string
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector, format_final_statement
 
@@ -5,6 +9,8 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    """Runs the emotion detection algorithm and handles empty re
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
@@ -16,7 +22,9 @@ def emotion_analyzer():
 
 @app.route("/")
 def render_index_page():
+    """Render index page
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
