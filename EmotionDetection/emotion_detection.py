97a34276ac2dca5a32bb37c6119a5337d3bc0acd
Alan Koncar <koncar.alan@gmail.com>
2025-06-15 22:23:12 -0400

    incorporated error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,6 +7,14 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json=input_json, headers=headers)
+    if response.status_code == 400:
+        no_text = {"anger": None, 
+                   "disgust": None, 
+                   "fear": None, 
+                   "joy": None, 
+                   "sadness": None, 
+                   "dominant_emotion": None}
+        return no_text
     return format_response(response.text)
 
 def format_response(response_to_format):
@@ -14,4 +22,23 @@ def format_response(response_to_format):
     formated_response = dictionary["emotionPredictions"][0]["emotion"]
     formated_response['dominant_emotion'] = max(formated_response, key=formated_response.get)
     return formated_response
+
+def format_final_statement(response):
+    dominant_emotion = response["dominant_emotion"]
+    
+    emotions = []
+    for item in response:
+        if item == "dominant_emotion":
+            continue
+        emotions.append(f"'{item}': {response[item]}")    
+    result_statement = ", ".join(emotions)
+
+    last_comma_index = result_statement.rfind(',')
+    result_statement = result_statement[:last_comma_index] + \
+                       ' and' + result_statement[last_comma_index + 1:]
+
+    final_statement = f"<p>For the given statement, the system response is {result_statement}. " \
+                      + f"The dominant emotion is <b>{dominant_emotion}</b>.</p>"
+    
+    return final_statement
 
--- server.py
+++ server.py
@@ -1,23 +1,22 @@
 from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector, format_final_statement
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
-    # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
-
-    # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
 
-    dominant_emotion = response["dominant_emotion"]
-    
-    return response
+    if response["dominant_emotion"] is None:
+        final_statement = " Invalid text! Please try again!"
+    else:
+        final_statement = format_final_statement(response)
+    return final_statement
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
